7f53b283dbb9308d8edd4137fc5bf4a86c53e738
mausayao <mauricio.sayao@gmail.com>
2019-06-03 22:30:57 -0400

    adicionado realm e realizado refatoração

---
+++ Todoey/model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Maurício de Freitas Sayão on 03/06/19.
+//  Copyright © 2019 Maurício de Freitas Sayão. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name: String = ""
+    let activities = List<Activity>()
+}
